0024f1232f061334dc0a22d3be3c07679b2fa55d
Ankit Chandnani <52832467+ankitchandnani@users.noreply.github.com>
2021-12-13 14:28:29 -0500

    Revert "merge to main for heroku app"

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -2,21 +2,10 @@ from flask import Blueprint, redirect, url_for, render_template, flash, request
 from flask_login import current_user, login_required, login_user, logout_user
 
 from .. import bcrypt, mail
-from ..forms import RegistrationForm, LoginForm, UpdateUsernameForm, EmailVerificationForm, UpdateDescriptionForm, UpdateProfilePicForm
+from ..forms import RegistrationForm, LoginForm, UpdateUsernameForm, EmailVerificationForm
 from ..models import User
 from flask_mail import Message
 from ..utils import gen_code
-from werkzeug.utils import secure_filename
-
-import io
-import base64
-
-def get_b64_img(username):
-    user = User.objects(username=username).first()
-    bytes_im = io.BytesIO(user.profile_pic.read())
-    image = base64.b64encode(bytes_im.getvalue()).decode()
-    return image
-
 
 users = Blueprint("users", __name__)
 
@@ -29,9 +18,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-        user = User(username=form.username.data, email=form.email.data, password=hashed,
-                    confirmed=False, code=gen_code(),
-                    profile_pic=None)
+        user = User(username=form.username.data, email=form.email.data, password=hashed, confirmed=False, code=gen_code())
         user.save()
 
         msg = Message(f"Your verification code is {user.code}",
@@ -76,36 +63,16 @@ def logout():
 @login_required
 def account():
     username_form = UpdateUsernameForm()
-    desc_form = UpdateDescriptionForm()
-    picture_form = UpdateProfilePicForm()
 
     if username_form.validate_on_submit():
         current_user.modify(username=username_form.username.data)
         current_user.save()
         return redirect(url_for("users.account"))
 
-    if desc_form.validate_on_submit():
-        current_user.modify(description=desc_form.desc.data)
-        current_user.save()
-        return redirect(url_for("users.account"))
-
-    if picture_form.validate_on_submit():
-        img = picture_form.picture.data
-        filename = secure_filename(img.filename)
-        content_type = f'images/{filename[-3:]}'
-
-        if current_user.profile_pic.get() is None:
-            current_user.profile_pic.put(img.stream, content_type=content_type)
-        else:
-            current_user.profile_pic.replace(img.stream, content_type=content_type)
-        current_user.save()
-        return redirect(url_for('users.account'))
-
     return render_template(
         "account.html",
         title="Account",
-        username_form=username_form, desc_form=desc_form, picture_form=picture_form,
-        image=get_b64_img(current_user.username)
+        username_form=username_form,
     )
 
 
