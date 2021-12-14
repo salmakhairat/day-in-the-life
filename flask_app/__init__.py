564c10066f723981444011e76599111bddb2c42c
Aaron Shavolian <ashavolian@gmail.com>
2021-12-13 23:27:36 -0500

    modified for heroku

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -53,11 +53,11 @@ def create_app(test_config=None):
 
     mail.init_app(app)
 
-    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
-
     app.config.from_pyfile("config.py", silent=False)
     if test_config is not None:
         app.config.update(test_config)
+
+    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 
     db.init_app(app)
     login_manager.init_app(app)
